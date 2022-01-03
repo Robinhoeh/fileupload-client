dea4585ebf6edce01382183ab8c1ea964931426b
robinwatson <drumsrobin@gmail.com>
2022-01-03 15:29:10 -0500

    add dynamic uploader file component to parent

--- src/uploader/components/Uploader.vue
+++ src/uploader/components/Uploader.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
     <UploaderForm @chosen="handleFilesChosen" />
+    <UploaderFile v-for="(upload, i) in uploads" :key="i" :upload="upload" />
   </div>
 </template>
 
 <script>
 import UploaderForm from "./UploaderForm";
+import UploaderFile from "./UploaderFile";
 
 export default {
   components: {
     UploaderForm,
+    UploaderFile,
+  },
+  data() {
+    return {
+      uploads: [],
+    };
   },
   methods: {
     handleFilesChosen(files) {
       console.log(files);
+      this.uploads.push(
+        ...Array.from(files).map((file) => {
+          return {file};
+        })
+      );
     },
   },
 };
