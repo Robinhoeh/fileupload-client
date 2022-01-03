fc578b2efdf6c3c73f30bc055814b8d6132a5922
robinwatson <drumsrobin@gmail.com>
2022-01-03 11:13:03 -0500

    Lets do it

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container" id="app">Home</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style src="@/assets/styles/app.css"></style>
