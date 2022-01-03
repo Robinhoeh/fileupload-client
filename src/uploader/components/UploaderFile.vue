049a24678a31d070c2d086d0b0e37cf4b6cd94e6
robinwatson <drumsrobin@gmail.com>
2022-01-03 15:28:41 -0500

    add uploader file component

---
+++ src/uploader/components/UploaderFile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="tw-bg-white tw-rounded-lg tw-p-4 tw-flex tw-items-stretch tw-mb-1"
+  >
+    <div>Progress</div>
+    <div class="tw-flex tw-flex-col tw-justify-between">
+      <div class="tw-mb-2">
+        <div
+          class="tw-font-medium tw-mr-3 tw-text-gray-700 tw-leading-tight"
+        ></div>
+        {{ upload.file.name }}
+      </div>
+      <div class="tw-text-gray-600 tw-text-sm tw-leading-tight">mb size</div>
+
+      <div class="tw-text-gray-600 tw-text-sm tw-align-baseline">Cancelled</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    upload: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
