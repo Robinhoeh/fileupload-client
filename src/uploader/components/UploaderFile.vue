913a0146484fb3d07311ffc7995dc1f61ca01273
robinwatson <drumsrobin@gmail.com>
2022-01-04 14:40:41 -0500

    add import states and two checks

--- src/uploader/components/UploaderFile.vue
+++ src/uploader/components/UploaderFile.vue
@@ -14,13 +14,26 @@
         {{ sizeDisplay }} MB
       </div>
 
-      <div class="tw-text-gray-600 tw-text-sm tw-align-baseline">Cancelled</div>
+      <div class="tw-text-gray-600 tw-text-sm tw-align-baseline">
+        <template v-if="state === states.WAITING">Waiting</template>
+        <template v-if="state === states.UNSUPPORTED"
+          >Sorry, this file type is unsupported</template
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import states from "@/uploader/states";
+
 export default {
+  data() {
+    return {
+      state: null,
+      states,
+    };
+  },
   props: {
     upload: {
       type: Object,
@@ -33,17 +46,19 @@ export default {
     endpoint: {
       required: true,
       //Removed the type check
-      //TODO:  add some sort of feedback for non supported files
+      //TODO:  add some sort of user feedback for non supported files
     },
-  },
-  mounted() {
-    console.log(this.baseURL);
-    console.log(this.endpoint);
   },
   computed: {
     sizeDisplay() {
       return (this.upload.file.size / 10000000).toFixed(2);
     },
+  },
+  mounted() {
+    if (this.endpoint === null) {
+      return (this.state = states.UNSUPPORTED);
+    }
+    this.state = states.WAITING;
   },
 };
 </script>
