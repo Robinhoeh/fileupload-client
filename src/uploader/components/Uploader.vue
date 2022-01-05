a36d8ea11268d91c66e5455e09733a5b078b4161
robinwatson <drumsrobin@gmail.com>
2022-01-05 11:25:41 -0500

    add logic for parent level access to progress

--- src/uploader/components/Uploader.vue
+++ src/uploader/components/Uploader.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    {{ overallProgress }}
+    {{ uploads }}
     <UploaderForm @chosen="handleFilesChosen" />
     <UploaderFile
       v-for="(upload, i) in uploads"
@@ -7,6 +9,8 @@
       :endpoint="determineEndpointFor(upload.file.type)"
       :upload="upload"
       :baseURL="options.baseURL"
+      @progress="handleUploadProgress"
+      @change="handleUploadChange"
     />
   </div>
 </template>
@@ -15,7 +19,9 @@
 import UploaderForm from "./UploaderForm";
 import UploaderFile from "./UploaderFile";
 import options from "@/uploader/options";
+import states from "@/uploader/states";
 import get from "lodash.get";
+import uuid from "uuid/v4";
 
 export default {
   components: {
@@ -38,6 +44,13 @@ export default {
       uploads: [],
     };
   },
+  computed: {
+    overallProgress() {
+      return parseInt(
+        this.uploads.reduce((a, b) => a + b.progress, 0) / this.uploads.length
+      );
+    },
+  },
   methods: {
     determineEndpointFor(fileType) {
       return get(this.handlers[fileType], "endpoint", null);
@@ -45,9 +58,57 @@ export default {
     handleFilesChosen(files) {
       this.uploads.push(
         ...Array.from(files).map((file) => {
-          return {file};
+          return {
+            // Show the overall progress of all uploads without mutating the prop in
+            // add uinque id to each induvidual upload
+            id: uuid(),
+            progress: 0,
+            uploading: false,
+            complete: false,
+            cancelled: false,
+            file,
+          };
         })
       );
+    },
+    handleUploadProgress(e) {
+      this.uploads = this.uploads.map((upload) => {
+        if (e.id === upload.id) {
+          // re-sets the custom object progress property
+          upload.progress = e.progress;
+        }
+        return upload;
+      });
+    },
+    handleUploadChange(e) {
+      switch (e.state) {
+        case states.UPLOADING:
+          this.uploads = this.uploads.map((upload) => {
+            if (e.id === upload.id) {
+              upload.uploading = true;
+            }
+            return upload;
+          });
+          break;
+        case states.COMPLETE:
+          this.uploads = this.uploads.map((upload) => {
+            if (e.id === upload.id) {
+              upload.complete = true;
+              upload.uploading = false;
+            }
+            return upload;
+          });
+          break;
+        case states.CANCELLED:
+          this.uploads = this.uploads.map((upload) => {
+            if (e.id === upload.id) {
+              upload.cancelled = true;
+              this.uploading = false;
+            }
+            return upload;
+          });
+          break;
+      }
     },
   },
 };
